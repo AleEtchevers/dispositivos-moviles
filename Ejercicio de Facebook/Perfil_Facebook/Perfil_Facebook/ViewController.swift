5a411cb55916de2a7ec1952d0f108ee728d250fb
YOUR NAME <aetchevers@eagerworks.com>
2022-03-31 22:22:08 -0300

    profile first view part
    - Fix the buttons

--- Ejercicio de Facebook/Perfil_Facebook/Perfil_Facebook/ViewController.swift
+++ Ejercicio de Facebook/Perfil_Facebook/Perfil_Facebook/ViewController.swift
@@ -9,8 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var circularProfilePic: UIImageView!
+    @IBOutlet weak var backProfilePic: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        circularProfilePic.layer.masksToBounds = true
+        circularProfilePic.layer.cornerRadius = circularProfilePic.bounds.width / 2
+        circularProfilePic.layer.borderColor = UIColor.white.cgColor
+        
+        circularProfilePic.contentMode = .scaleToFill
+        circularProfilePic.layer.borderWidth = 5
         // Do any additional setup after loading the view.
     }
 
